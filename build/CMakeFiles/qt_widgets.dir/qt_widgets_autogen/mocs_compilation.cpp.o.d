4e2a6ccde983db2705ac9b98e6130d71a94e3902
ManveerSingh0 <singhcoder2005@gmail.com>
2025-02-09 18:50:38 +0530

    removed the clutter

--- build/CMakeFiles/qt_widgets.dir/qt_widgets_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qt_widgets.dir/qt_widgets_autogen/mocs_compilation.cpp.o: \
- /home/manveer/programming/qt_applications/learning/build/qt_widgets_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
