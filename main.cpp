20fb2b089cddf90390404fd1e7d64dd68d4fcc3a
ManveerSingh0 <singhcoder2005@gmail.com>
2025-03-26 15:26:22 +0530

    getting comfortable with qt

--- main.cpp
+++ main.cpp
@@ -5,18 +5,11 @@
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
+
   QWidget window;
   window.resize(230, 240);
-  window.setWindowTitle(QApplication::translate("Calculator",
-                                                "Manveer Singh"));
+  window.setWindowTitle("Calculator");
   window.show();
-
-
-  QPushButton *button = new QPushButton(
-      QApplication::translate("childwidget", "Press Me"), &window);
-
-  button->move(100, 100);
-  button->show();
 
   return app.exec();
 }
