b7695f7358bfa0f3e288812ec12563e6138f0672
ManveerSingh0 <singhcoder2005@gmail.com>
2025-03-30 21:06:12 +0530

    completed the tutorial

--- calculator.h
+++ calculator.h
@@ -1,13 +1,66 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-#include <QWidget>
+#include "button.h"
 
+
+
+#include <QWidget>
+#include <QToolButton>
+#include <QLineEdit>
+#include <QFont>
+#include <QGridLayout>
 
 class Calculator : public QWidget{
   Q_OBJECT
-public slots:
-  void digitClicked();
+public:
+  Calculator(QWidget* parent = nullptr);
+  ~Calculator();
+
+
+private slots:
+    void digitClicked();
+    void unaryOperatorClicked();
+    void additiveOperatorClicked();
+    void multiplicativeOperatorClicked();
+    void equalClicked();
+    void pointClicked();
+    void changeSignClicked();
+    void backspaceClicked();
+    void clear();
+    void clearAll();
+    void clearMemory();
+    void readMemory();
+    void setMemory();
+    void addToMemory();
+
+
+
+
+private:
+
+   template <class pointerToMemberFunction>
+   Button* createButton(const QString &text, const pointerToMemberFunction& member);
+  void abortOperation();
+  bool calculate(double rightOperand, const QString &pendingOperator);
+
+  //Variable which contain calculated data and when to clear that data
+  double sumInMemory;
+  double sumSoFar;
+  double factorSoFar;
+  QString pendingAdditiveOperator;
+  QString pendingMultiplicativeOperator;
+  bool waitingForOperand;
+
+
+  //this will be used in creating buttons from 0-9 in calculator 
+  QLineEdit* display;
+  QFont font; 
+  enum {
+    NumDigitButtons = 10
+  };
+  
+  Button* digitButtons[NumDigitButtons];
 };
 
 
--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 
 
 
-
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
-
+  Calculator window;
+  window.show();
 
   return app.exec();
+
 }
