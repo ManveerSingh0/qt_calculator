197a486e15f3e96d06dc0a12033e388bc6d8c059
ManveerSingh0 <singhcoder2005@gmail.com>
2025-03-30 19:03:57 +0530

    building first project while live streaming on youtube

--- calculator.h
+++ calculator.h
@@ -2,21 +2,12 @@
 #define CALCULATOR_H
 
 #include <QWidget>
-#include <QLineEdit>
-#include <QGridLayout>
+
 
 class Calculator : public QWidget{
   Q_OBJECT
- public:
-  Calculator(QWidget* parent = nullptr);
-
-  void show();  
-
-
- private:
-  QLineEdit* line_edit;
-  QGridLayout* grid;
-
+public slots:
+  void digitClicked();
 };
 
 
