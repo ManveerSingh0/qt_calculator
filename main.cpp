ce2c108c004a99b01aba36a15b35ff671bdee4a5
ManveerSingh0 <singhcoder2005@gmail.com>
2025-12-24 18:50:39 +0530

    everything setup , consistency from 25th nice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <print>
+#include <QtWidgets>
 
 
 int main(int argc, char *argv[]) {
-  std::println("Hello World");
+  QApplication app(argc, argv);
+  QWidget* window = new QWidget();
+
+  window->resize(300,300);
+  window->show();
+
+
+  int exit_code = app.exec();
+
+
+  delete window;
+  window = nullptr;
+
+
+  return exit_code;
 }
